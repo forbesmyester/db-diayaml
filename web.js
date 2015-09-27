29c9c16d085348a1658a8bac712005ef7df53d42
Matthew Forrester <github@speechmarks.com>
2015-09-27 11:14:05 +0100

    Better feedback

--- web.js
+++ web.js
@@ -10,22 +10,29 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/yaml");
 
+function drawError(msg) {
+    document.getElementById("diagram").innerHTML = '<div class="error"><h2>Error</h2><p>There was an error in ' + msg + '</p></div>';
+}
+
 function redraw() {
     var json = {};
     try {
         json = jsyaml.safeLoad(editor.getValue());
     } catch (e) {
-        document.getElementById("diagram").innerHTML = '<div class="error"><h2>Error</h2><p>Your YAML does not appear to be valid</p></div>';
-        return;
-    }
-    if (console && console.log) {
-        console.log("RENDERING: ", lib.getDotSrc(lib.transform(json)).join("\n"));
+        return drawError('YAML conversion');
     }
     /* eslint new-cap: 0 */
-    document.getElementById("diagram").innerHTML = Viz(
-        lib.getDotSrc(lib.transform(json)).join("\n"),
-        "svg"
-    );
+    try {
+        document.getElementById("diagram").innerHTML = Viz(
+            lib.getDotSrc(lib.transform(json)).join("\n"),
+            "svg"
+        );
+    } catch (e) {
+        drawError('generated GraphViz/DOT source, see console');
+        if (console && console.log) {
+            console.log("ERROR IN DOT SRC: ", lib.getDotSrc(lib.transform(json)).join("\n"));
+        }
+    }
 }
 
 redraw();
